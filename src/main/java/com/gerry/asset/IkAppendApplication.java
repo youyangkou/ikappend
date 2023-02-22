7543956452ad5e018b094eae4664189e95369188
Gerry <1025063342@qq.com>
2023-02-22 16:51:22 +0800

    springboot版本升级，resources文件夹位置变更

--- src/main/java/com/gerry/asset/IkAppendApplication.java
+++ src/main/java/com/gerry/asset/IkAppendApplication.java
@@ -2,8 +2,6 @@ package com.gerry.asset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -16,17 +14,4 @@ public class IkAppendApplication
     public static void main(String[] args) {
         SpringApplication.run(IkAppendApplication.class, args);
     }
-
-    /**
-     * 修改tomcat默认端口
-     * @return
-     */
-    @Bean
-    public TomcatEmbeddedServletContainerFactory servletContainer(){
-        TomcatEmbeddedServletContainerFactory container = new TomcatEmbeddedServletContainerFactory();
-        container.setPort(8081);
-        return container;
-    }
-
-
 }
